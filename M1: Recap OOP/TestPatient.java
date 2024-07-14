73421e680334197dcf454ae3cdf8615fad75f1f8
neriah <144508337+neriahv@users.noreply.github.com>
2024-07-15 07:09:57 +0800

    Update TestPatient.java

--- M1: Recap OOP/TestPatient.java
+++ M1: Recap OOP/TestPatient.java
@@ -9,7 +9,7 @@ public class TestPatient {
         neriah.setPatientID(1);
         neriah.setName("Neriah Villapana");
         neriah.setConsultations(date1);
-        neriah.doctorName("Dr. Sy");
+        neriah.doctorName = "Dr. Sy";
         neriah.printPatientDetails();
 
         String[] date2 = new String[]{"June 8, 2020", "July 9, 2020"};
@@ -17,7 +17,7 @@ public class TestPatient {
         faith.setPatientID(2);
         faith.setName("Faith Lopez");
         faith.setConsultations(date2);
-        faith.doctorName("Dr. Belo");
+        faith.doctorName = "Dr. Belo";
         faith.printPatientDetails();
     }
 }
